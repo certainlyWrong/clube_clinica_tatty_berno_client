b1df5851f412ad288025f3257c8854290a94832f
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-11-05 14:48:01 -0300

    [LoopCLI]: Client molded by metadata

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   runApp(
     AppWidget(
       endpoints: LoopCoreEndpoints(
-        clientDomain: 'demo.loopmais.com.br',
+        clientDomain: 'clube.clinicatattyberno.com.br',
         apiUrl: 'https://api.loopmais.net.br/api/v1',
         storageUrl: 'https://storage.googleapis.com/loopmais',
       ),
